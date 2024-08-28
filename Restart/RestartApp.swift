2908aad40f78eca0e6e6aef24f946ebe1a3f43a7
Furkan İSLAM <65629988+furkanislam@users.noreply.github.com>
2024-08-28 15:20:01 +0300

    Add files via upload

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Furkan İSLAM on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,25 @@
+ //
+//  ContentView.swift
+//  Restart
+//
+//  Created by Furkan İSLAM on 27.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
